ab1576591dc32c64eb81fb500326b2d90b8b7f24
Arshiyan Ali <155827717+Arshiyan-ali@users.noreply.github.com>
2024-07-10 17:43:35 -0700

    Add files via upload

---
+++ practice.dart
@@ -0,0 +1,13 @@
+void main(){
+  int total = 100;
+  int marks = 50;
+  if(marks > 60 && marks < 70){
+    print('B grade');
+  }else if(marks >70 && marks <80){
+    print('A garde');
+  }else if(marks >90){
+    print('A one grade');
+  }else {
+    print('you are failed');
+  }
+  }
